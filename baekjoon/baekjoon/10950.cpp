eb025a11d325904da16aadc39feaaf7b9a2fa046
OkiHeo <37202612+OkiHeo@users.noreply.github.com>
2019-07-20 22:08:50 +0900

    190720 commit

---
+++ baekjoon/baekjoon/10950.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int T;
+	cin >> T;		// number of testCase
+
+	while (T--) {
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << endl;
+	}
+
+	return 0;
+}
