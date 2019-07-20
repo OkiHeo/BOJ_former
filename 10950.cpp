6b92b23cd30976379b178f3254cdf487b345182d
OkiHeo <37202612+OkiHeo@users.noreply.github.com>
2019-07-20 22:10:38 +0900

    Update 10950.cpp

--- 10950.cpp
+++ 10950.cpp
@@ -3,8 +3,13 @@ using namespace std;
 
 int main(void) {
 	int T;
+<<<<<<< HEAD
 	cin >> T;		// number of testCase
 
+=======
+	cin >> T;		// 테스트 케이스의 개수
+	
+>>>>>>> parent of abb002b... 19/7/20
 	while (T--) {
 		int A, B;
 		cin >> A >> B;
